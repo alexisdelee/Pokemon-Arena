945445dbb63dec384e75d950a78c6e9e74557652
Arthur Péchayre <apechayre@wedigital.garden>
2020-02-21 01:40:25 +0100

    Repair type modifier

--- src/app/combat/combat.service.ts
+++ src/app/combat/combat.service.ts
@@ -1,14 +1,11 @@
 import { Injectable } from '@angular/core';
-
 import { CombatState } from './CombatState';
-import { Move } from '../pokemon/move.model';
-
 import { PokemonService } from '../pokemon/pokemon.service';
 import {Intent} from './turn-order.model';
 import {Pokemon} from '../pokemon/pokemon.model';
 import {Type} from '../pokemon/type.model';
-import {forkJoin, Observable, zip} from 'rxjs';
-import {flatMap, map} from 'rxjs/operators';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -65,13 +62,17 @@ export class CombatService {
         let m = 1;
         target.types.forEach((targetType) => {
           if (attackType.damage_relations.double_damage_to
-            .find((typeRelation) => typeRelation.name === targetType.name)) {
+            .find((typeRelation) => typeRelation.name === targetType.type.name)) {
             m *= 2;
-          } else if (attackType.damage_relations.half_damage_to
-            .find((typeRelation) => typeRelation.name === targetType.name)) {
+          }
+
+          if (attackType.damage_relations.half_damage_to
+            .find((typeRelation) => typeRelation.name === targetType.type.name)) {
             m *= 0.5;
-          } else if (attackType.damage_relations.no_damage_to
-            .find((typeRelation) => typeRelation.name === targetType.name)) {
+          }
+          
+          if (attackType.damage_relations.no_damage_to
+            .find((typeRelation) => typeRelation.name === targetType.type.name)) {
             m *= 0;
           }
         });
@@ -102,54 +103,4 @@ export class CombatService {
 
     return dmg;
   }
-
-/*
-  async function gameLoop(poke1: Pokemon, poke2: Pokemon) :Promise<Pokemon|undefined> {
-    poke1.hp = 100;
-    poke2.hp = 100;
-    var i :number = 1;
-
-    while (1) {
-      console.log(`Tour ${i++}\n${poke1.name} : ${poke1.hp} HP <=> HP ${poke2.hp} : ${poke2.name}`);
-      const moves :Move[] = await Promise.all([pickRandomMove(poke1.moves), pickRandomMove(poke2.moves)]);
-
-      var first = whoMovesFirst(poke1, poke2, moves[0], moves[1]);
-      var second = poke1 === first ? poke2 : poke1;
-
-      const firstMove = first === poke1 ? moves[0] : moves[1];
-      const secondMove = first === poke1 ? moves[1] : moves[0];
-
-      console.log(`${first.name} hits first whith ${firstMove.name} !`);
-      console.log(`${second.name} loose ${firstMove.power ?? 'no'} PV !`);
-      second.hp -= firstMove.power;
-      if (second.hp <= 0) {
-        return second;
-      }
-
-      console.log(`${second.name} hits back whith ${secondMove.name} !`);
-      console.log(`${first.name} loose ${secondMove.power ?? 'no'} PV !`);
-      first.hp -= secondMove.power;
-      if (first.hp <= 0) {
-        return first;
-      }
-      sleep(3000);
-    }
-
-    return undefined;
-  }
-
-  export async function game(duration: number = -1) {
-    for (;duration != 0; --duration) {
-      console.log("================ Get 2 randoms pokemons =============================");
-      const pokes :Pokemon[] = await Promise.all([getNewPokemon(), getNewPokemon()]) ;
-      console.log(pokes[0].name + " VS " + pokes[1].name);
-      const ko = await gameLoop(pokes[0], pokes[1]);
-      if (!ko) {
-        return;
-      }
-      console.log(`------ ${ko.name} is KO ! ------`);
-    }
-  }
-
- */
 }
